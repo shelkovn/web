cfd3bb401382e444b779c4e952b5692729707891
towa <316>
2026-04-30 12:11:50 +0700

    представление функций на html странице

--- l5/script.js
+++ l5/script.js
@@ -1,88 +1,94 @@
-//Задание 1.
-//развернуть число
+// ФУНКЦИИ
+
 function reverse(n) {
-    let a = String(n)
-        .split('')   // to array
-        .reverse()   // backward order
-        .join('');   // to string
-    
-    return Number(a);
+    return Number(String(n).split('').reverse().join(''));
 }
-console.log(reverse(123));
 
-//оставить только уникальные цифры
 function unique(n) {
-    let a = [...new Set(String(n))].join('');  
-    return Number(a);
+    return Number([...new Set(String(n))].join(''));
 }
-console.log(unique(111333456));
 
-//посчитать вхождения цифры
 function count(n, x) {
     let count = 0;
-
+    let target = String(x)[0];
     for (let char of String(n)) {
-        if (char === String(x)[0]) count++;
+        if (char === target) count++;
     }
     return count;
 }
-console.log(count(1355567, 5));
 
-//Посчитать самую длинную последовательность нулей/единиц в двоичной записи данного числа.
 function countbinary(n) {
     let binary = Number(n).toString(2);
-    let c, maxcount = 0;
+    let c = 0, maxcount = 0; 
     let last = binary[0];
 
-    for (let char of String(binary)) {
+    for (let char of binary) {
         if (char === last) {
             c++;
-        }
-        else {
-            if (c > maxcount)
-                maxcount = c;
+        } else {
+            if (c > maxcount) maxcount = c;
             c = 1;
             last = char;
         }
     }
-    if (c > maxcount)
-        maxcount = c;
-    return maxcount;
+    return Math.max(c, maxcount);
 }
-console.log(countbinary(101024)); //1 10001010 10100000 -> 5
 
-//Задание 2
-//Найти самый первый неповторяющийся символ в строке: 'фывфавыапрс' -> 'п'.
-function firstunique(str)
-{
+function firstunique(str) {
     let counts = {};
-
     for (let char of str) {
         counts[char] = (counts[char] || 0) + 1;
     }
-
-    return Object.keys(counts).find(key => counts[key] === 1);
+    return Object.keys(counts).find(key => counts[key] === 1) || 'Нет таких';
 }
-console.log(firstunique('фывфавыапрс'))
 
-
-//Cгенерировать строку заданной длины из случайных символов, взятых из набора английскийх букв и цифр: (5) -> '2fvg6'.
 function random(length) {
     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let result = '';
-    
     for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor((Math.random()) * chars.length);
-        result += chars[randomIndex];
+        result += chars[Math.floor(Math.random() * chars.length)];
     }
-    
     return result;
 }
-console.log(random(5))
 
-//Вернуть только уникальные символы строки: 'позволяеткопироватьтекстиз' -> 'позвляеткираьс'.
 function uniquestr(n) {
-    let a = [...new Set(String(n))].join('');  
-    return a;
+    return [...new Set(String(n))].join('');
 }
-console.log(uniquestr('позволяеткопироватьтекстиз'));
+
+// UI
+
+function uiReverse() {
+    const val = document.getElementById('inputReverse').value;
+    document.getElementById('resReverse').innerText = reverse(val);
+}
+
+function uiUnique() {
+    const val = document.getElementById('inputUnique').value;
+    document.getElementById('resUnique').innerText = unique(val);
+}
+
+function uiCount() {
+    const n = document.getElementById('inputCountN').value;
+    const x = document.getElementById('inputCountX').value;
+    document.getElementById('resCount').innerText = count(n, x);
+}
+
+function uiBinary() {
+    const val = document.getElementById('inputBin').value;
+    document.getElementById('resBin').innerText = `${countbinary(val)} (for ${Number(val).toString(2)})`;
+}
+
+function uiFirstUnique() {
+    const val = document.getElementById('inputFirstUnique').value;
+    document.getElementById('resFirstUnique').innerText = firstunique(val);
+}
+
+function uiRandom() {
+    const val = document.getElementById('inputRandLen').value;
+    document.getElementById('resRandom').innerText = random(val);
+}
+
+function uiUniqueStr() {
+    const val = document.getElementById('inputUniqueStr').value;
+    document.getElementById('resUniqueStr').innerText = uniquestr(val);
+}
